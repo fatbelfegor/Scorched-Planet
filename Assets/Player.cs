fe96976854fd94c4e9c7c5d982a880cc8ea6b871
Klim Agarkov <jewelgatorstv@gmail.com>
2018-07-06 00:05:55 +0300

    Add player ship speed and screeen limit

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,8 +15,11 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-		
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime; // x - axis ship movement in the cerrent frame
+        float rawXPos = transform.localPosition.x + xOffset; // new X position
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
