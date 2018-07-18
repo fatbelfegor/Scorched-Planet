2f2ce5e476600c8a89ab89786af0c50d6f2fc894
Klim Agarkov <jewelgatorstv@gmail.com>
2018-07-19 02:12:02 +0300

    Add collision handler

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // ok as long as this is the only script that loads scenes
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        print("Player dying");
+        SendMessage("OnPlayerDeath");
+    }
+}
