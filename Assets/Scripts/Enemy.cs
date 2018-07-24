ecd62609144ed1d1135f9b5e36608b6dd6710854
Klim Agarkov <jewelgatorstv@gmail.com>
2018-07-24 22:28:24 +0300

    Add enemy colliders

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AddNonTriggerBoxCollider();
+		
+	}
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Destroy(gameObject);
+    }
+}
