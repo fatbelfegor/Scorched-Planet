33cdace73387d3dbbc0cae5532de488f95316588
Klim Agarkov <jewelgatorstv@gmail.com>
2018-08-15 22:54:32 +0300

    Finish enemy explosion

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 
 	// Use this for initialization
 	void Start () {
@@ -26,6 +27,8 @@ public class Enemy : MonoBehaviour {
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
