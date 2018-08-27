b1aa95f1e8091dbb6378a05d8eeb8e9eda4adc72
Klim Agarkov <jewelgatorstv@gmail.com>
2018-08-27 23:19:05 +0300

    Add enemy timeline.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,35 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 12;
-    [SerializeField] int healthPoints = 100;
+    [SerializeField] int hits = 3;
 
     ScoreBoard scoreBoard;
 
-	// Use this for initialization
-	void Start () {
-        AddNonTriggerBoxCollider();
+    // Use this for initialization
+    void Start()
+    {
+        AddBoxCollider();
         scoreBoard = FindObjectOfType<ScoreBoard>();
-	}
+    }
 
-    private void AddNonTriggerBoxCollider()
+    private void AddBoxCollider()
     {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+    }
 
-    private void OnParticleCollision(GameObject other)
+    private void ProcessHit()
     {
         scoreBoard.ScoreHit(scorePerHit);
+        hits = hits - 1;
+        // todo consider hit FX
+    }
+
+    private void KillEnemy()
+    {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
